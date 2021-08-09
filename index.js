76ea34b7bfd0b45ba9e8607f0cf75cffec3637ed
standupplease <a402021@gmail.com>
2021-08-09 23:32:21 +0800

    321

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
-const apiEndpoint = 'https://ptx.transportdata.tw/MOTC/v2'
+const apiEndpoint = 'https://ptx.transportdata.tw/MOTC/v2/Rail/THSR/'
 
 const searchAllStationsBtn = document.getElementById('searchAllStations')
 
+const queryTrainInfoBtn = document.getElementById('queryTrainInfo')
+const originStation = document.getElementById('originStation')
+const destinationStation = document.getElementById('destinationStation')
+
+queryTrainInfoBtn.addEventListener('click', async function(){
 
 
+    let origin = originStation.value
+    let destination = destinationStation.value
 
+    console.log(origin)
+    console.log(destination)
 
-searchAllStationsBtn.addEventListener('click', function () {
-   
-    doGetAllStation()
-    let stationList = document.getElementById('stationsList')
-    let stationListAry = 
-    // let str = ""
-    // str+=`
-    //         <p>站名 : </p>
-    //         <p>站碼 : </p>
-    // `
+    let date = new Date()
+    let yyyyMMDD = date.toISOString().substring(0,10)
+    let queryResultList = await queryTrainInfomation(origin,destination,yyyyMMDD)
 
-    // listStation.forEach(element => {
-        
-    // });
+    console.log(queryResultList)
 
+    let HTMLstr=""
 
+    queryResultList.forEach(element => {
+        HTMLstr+=
+        `
+        <p>起始 :${element.OriginStopTime.StationName.Zh_tw} 時間 : ${element.OriginStopTime.ArrivalTime}</p>
+        <p>到達 :${element.DestinationStopTime.StationName.Zh_tw} 時間 : ${element.DestinationStopTime.ArrivalTime}</p>
+        <p>***************************************</p>
+        `
+    });
 
-
-    // stationList.innerHTML =str
-
-    console.log(stationList)
-        
+    const queryTrainInfoList = document.getElementById('queryTrainInfoList')
+    queryTrainInfoList.innerHTML = HTMLstr
+    // <p> 起始:南港 時間 -> 到達:台中 時間
+    //<p>起始 :${item.OriginStopTime.StationName.Zh_tw} 時間 : {item.OriginStopTime.ArrivalTime}</p>
+    //<p>起始 :${item.DestinationStopTime.StationName.Zh_tw} 時間 : {item.DestinationStopTime.ArrivalTime}</p>
 })
 
+async function queryTrainInfomation(originStation,destination,yyyyMMDD){
+    
+    let url = `${apiEndpoint}/DailyTimetable/OD/${originStation}/to/${destination}/${yyyyMMDD}?$top=30&$format=JSON`
+    console.log(url)
+    let data
+    //https://ptx.transportdata.tw/MOTC/v2/DailyTimetable/OD/0990/to/1020/2021-08-09?$top=30&$format=JSON
 
-async function doGetAllStation() {
-    let url = `${apiEndpoint}/Rail/THSR/Station`
-    let data 
-    fetch(url, { method: 'GET' }).then(
+    //https://ptx.transportdata.tw/MOTC/v2/Rail/THSR/DailyTimetable/OD/1000/to/1047/2021-08-09?$top=30&$format=JSON
+    await fetch(url, { method: 'GET' }).then(
         res => {
             return res.json()
         }
@@ -44,7 +57,57 @@ async function doGetAllStation() {
         console.log(`error : ${error}`)
     })
 
+    return data
+}
 
-    console.log(data)
+
+
+
+
+
+
+searchAllStationsBtn.addEventListener('click', async function () {
+   
+    let stationListAry = await doGetAllStation()
+    let stationList = document.getElementById('stationsList')
+    console.log(stationListAry)
+
+
+    let HTMLstr = ""
+
+    stationListAry.forEach(element => {
+        HTMLstr+=
+        `
+             <p>站名 : ${element.StationName.Zh_tw}</p>
+             <p>站碼 : ${element.StationID}</p>
+        `
+    });
+   
+
+
+    stationList.innerHTML = HTMLstr
+
+    console.log(stationList)
+        
+})
+
+
+async function doGetAllStation() {
+    let url = `${apiEndpoint}/Station`
+
+    let data
+    await fetch(url, { method: 'GET' }).then(
+        res => {
+            return res.json()
+        }
+    ).then((jsonData) => {
+        data = jsonData
+
+    }).catch((error) => {
+        console.log(`error : ${error}`)
+    })
+
+
+    return data
 }
 
